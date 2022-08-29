c85bcaf87efff5b1279d113aba3c3813b2f83d92
Jen-GM <jen.gonzalez.mu@gmail.com>
2022-08-29 22:05:54 +0000

    domainGenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = domainGenerator(
+    pronoun,
+    adj,
+    noun,
+    ext
+  );
 };
+var domainGenerator = (pron, adje, sust, exten) => {
+  for (let i = 0; i < pron.length; i++) {
+    for (let j = 0; j < adje.length; j++) {
+      for (let k = 0; k < sust.length; k++) {
+        for (let l = 0; l < exten.length; l++) {
+          storeArrays.push("www." + pron[i] + adje[j] + sust[k] + exten[l]);
+          //return "www." + pron[i] + adje[j] + sust[k] + exten[l];
+        }
+      }
+    }
+  }
+  /* for (const key in storeArrays) {
+    return storeArrays[key] + " <br > ";
+  } */
+
+  return storeArrays;
+};
+const pronoun = ["the", "our", "this", "yours"];
+const adj = ["great", "big", "small", "stinky"];
+const noun = ["jogger", "racoon", "door", "cat"];
+const ext = [".com", ".org", ".es"];
+const storeArrays = [];
+
+console.log(domainGenerator);
